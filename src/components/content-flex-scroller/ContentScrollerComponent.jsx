1764aacbcf5a33731fe02210e7fefeac0f406132
Chris Lau Heredia <lau_heredia_@hotmail.com>
2020-09-06 19:12:35 -0500

    Sort select

--- src/components/content-flex-scroller/ContentScrollerComponent.jsx
+++ src/components/content-flex-scroller/ContentScrollerComponent.jsx
@@ -7,6 +7,7 @@ import "./styles.scss";
 const ContentScrollerComponent = props => {
   const { title, hasBackground, customParams, errorMsg="" } = props;
   const [movies, setMovies] = useState([]);
+  const [sortBy, setSortBy] = useState("title-asc");
 
   useEffect( () => {
     getMovies()
@@ -14,20 +15,49 @@ const ContentScrollerComponent = props => {
 
   // Get Shows data
   const getMovies = async () => {
-    const newMovies = await getCustomQuery(customParams);
+    let newMovies = await getCustomQuery(customParams);
     setMovies(newMovies ? movies.concat(newMovies) : movies);
+  }
+
+  // Sort by function
+  const sortMovies = (arr) => {
+    if (!sortBy) return arr
+    return arr.sort(function(a, b){
+      if(sortBy.includes("title")){
+        const firstItem = a.title, secondItem = b.title;
+        if (firstItem < secondItem) return sortBy.includes("asc") ? -1 : 1
+        if (firstItem > secondItem) return sortBy.includes("asc") ? 1 : -1
+        return 0;
+      } else {
+        const firstItem = a.vote_average, secondItem = b.vote_average;
+        return sortBy.includes("asc") ? firstItem - secondItem : secondItem - firstItem
+      }
+    });
   }
 
   // Returns array with cards
   const createCardsList = () => (
-    movies.map(movie => {
+    sortMovies(movies).map(movie => {
       return movie.poster_path ? <CardComponent movieInfo={movie} key={movie.id} /> : null
     })
   )
-  
+
+  // Select handler
+  const changeSorting = event => {
+    setSortBy(event.target.value)
+  }
+
   return movies.length > 0 ? (
     <div className={`content-scroller-container ${hasBackground && 'container-bg-color'}`}>
-      <h2>{title}</h2>
+      <div className="title-container">
+        <h2>{title}</h2>
+        <select id="sort" className="sort-select" onChange={e => changeSorting(e)} value={sortBy}>
+          <option value="title-asc">Title &#8593;</option>
+          <option value="title-desc">Title &#8595;</option>
+          <option value="votes-asc">Votes &#8593;</option>
+          <option value="votes-desc">Votes &#8595;</option>
+        </select>
+      </div>
       <div className="content-flex-scroller">
         <Suspense fallback={
           <div className="content-flex-scroller-loader-container">
